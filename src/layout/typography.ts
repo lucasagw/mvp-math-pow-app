dd22af69891f72ab1bc4ff5c46e3b7abefe6cb01
Lucas aUGUSTO <79553621+lucasagw@users.noreply.github.com>
2023-11-06 09:46:37 -0300

    ✨ created typography

---
+++ src/layout/typography.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Heading = styled.Text`
+  font-size: ${({ theme }) => theme.font.big};
+  color: ${({ theme }) => theme.color.black};
+`;
