a0579f62670bf9e81d65f7ce048dba89b3fa8556
Lucas aUGUSTO <79553621+lucasagw@users.noreply.github.com>
2023-11-06 16:32:42 -0300

    💄 added some colors

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,6 +2,9 @@ const theme = {
   color: {
     white: 'rgba(255, 255, 255,0.9)',
     black: '#4B4B4B',
+    red: '#FF5555',
+    green: '#50FA7B',
+    yellow: '#F1FA8C',
     primary: {
       main: '#4356F3',
       contrastText: '#F2F2F2',
@@ -23,6 +26,12 @@ const theme = {
     radius: '15px',
     style: '2px solid #736681',
   },
+  radius: {
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+    extraLarge: '36px',
+  },
   padding: {
     small: '8px',
     medium: '16px',
@@ -30,10 +39,16 @@ const theme = {
     extraLarge: '36px',
   },
   font: {
-    small: '8px',
+    small: '12px',
     default: '16px',
     big: '24px',
     bigger: '32px',
+  },
+  margin: {
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+    extraLarge: '36px',
   },
 };
 
