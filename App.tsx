58bdd1bc99d4db4dd9f063299c1b780868f75366
Lucas aUGUSTO <79553621+lucasagw@users.noreply.github.com>
2023-11-06 09:43:59 -0300

    ✨ added theme provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 // Components
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
 // Styles
 import * as S from './src/layout/containers';
+import theme from './src/theme/theme';
+import Home from './src/screens/Home';
 
 const App = () => {
   return (
-    <S.SafeViewContainer>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <ExpoStatusBar style="auto" />
-    </S.SafeViewContainer>
+    <ThemeProvider theme={theme}>
+      <S.SafeViewContainer>
+        <Home />
+        <ExpoStatusBar style="auto" />
+      </S.SafeViewContainer>
+    </ThemeProvider>
   );
 };
 
