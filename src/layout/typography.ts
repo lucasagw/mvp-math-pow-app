2ccaf31cc1bbab9e92d8d39db6bc62fccd649c4e
Lucas aUGUSTO <79553621+lucasagw@users.noreply.github.com>
2023-11-06 16:42:49 -0300

    💄 added colors

--- src/layout/typography.ts
+++ src/layout/typography.ts
@@ -1,6 +1,19 @@
+import { TextProps } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Heading = styled.Text`
+interface CustomProps {
+  variant?: 'white' | 'black';
+}
+
+type Props = TextProps & CustomProps;
+
+export const Heading = styled.Text<Props>`
   font-size: ${({ theme }) => theme.font.big};
   color: ${({ theme }) => theme.color.black};
+`;
+
+export const Text = styled.Text<Props>`
+  font-size: ${({ theme }) => theme.font.default};
+  color: ${({ theme, variant }) =>
+    (variant && theme.color[variant]) || theme.color.black};
 `;
